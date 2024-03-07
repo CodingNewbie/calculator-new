a6ba80951fa734129b81a544064c33b98f201ac0
Adam <adamrayschmidt@gmail.com>
2024-03-06 19:26:24 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+const clear = document.querySelector("#clear");
+const erase = document.querySelector("#erase");
+const percent = document.querySelector("#percent");
+const divide = document.querySelector("#divide");
+const multiply = document.querySelector("#multiply");
+const subtract = document.querySelector("#subtract");
+const decimal = document.querySelector("#decimal");
+const equals = document.querySelector("#equals");
+const add = document.querySelector("#add");
+const zero = document.querySelector("#0");
+const one = document.querySelector("#1");
+const two = document.querySelector("#2");
+const three = document.querySelector("#3");
+const four = document.querySelector("#4");
+const five = document.querySelector("#5");
+const six = document.querySelector("#6");
+const seven = document.querySelector("#7");
+const eight = document.querySelector("#8");
+const nine = document.querySelector("#9");
+
+
+
+
+
